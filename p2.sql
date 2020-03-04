633ceaf42e23319d037072af81e3fa7502175244
Antonio Vega <a.vegaramirez76@gmail.com>
2020-03-04 22:40:19 +0900

    p2 format

--- p2.sql
+++ p2.sql
@@ -3,17 +3,18 @@ WITH t1 AS (
         fil.title AS title,
         cat.name AS name,
         fil.rental_duration AS rental_duration,
-            NTILE(4) OVER (
-        PARTITION BY cat.name
-        ORDER BY
-            fil.rental_duration
-    ) AS standard_quartile
+        NTILE(4) OVER (
+            PARTITION BY cat.name
+            ORDER BY
+                fil.rental_duration
+        ) AS standard_quartile
     FROM
         category AS cat
         JOIN film_category AS film_cat ON cat.category_id = film_cat.category_id
         JOIN film AS fil ON film_cat.film_id = fil.film_id
 )
-SELECT *
+SELECT
+    *
 FROM
     t1
 WHERE
@@ -23,4 +24,5 @@ WHERE
     OR t1.name = 'Comedy'
     OR t1.name = 'Family'
     OR t1.name = 'Music'
-ORDER BY 3
+ORDER BY
+    3
